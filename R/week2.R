5459a0ef60258c658b456caf4f593fa717779d46
“Chulin <chen6496@umn.edu>
2023-01-25 18:49:25 -0600

    create factors

--- R/week2.R
+++ R/week2.R
@@ -1,3 +1,4 @@
 # Import and Labeling
 rt_df <- read.csv("data/week2.csv",header = TRUE)
-
+factor_condition <- factor(week2$condition, labels = c("Control", "Experimental"))
+factor_gender <- factor(week2$gender, labels = c("Female", "Male", "Nonbinary"))
