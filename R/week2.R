5b544fe9c00babfa3a6e6bc7a4ef4927ceaf65bf
[chulinchen20] <[chen6496@umn.edu]>
2024-01-29 04:40:43 -0600

    Update

--- R/week2.R
+++ R/week2.R
@@ -1,12 +1,12 @@
 # Import and Labeling
-rt_df <- read.csv("data/week2.csv",header = TRUE)
-factor_condition <- factor(week2$condition, labels = c("Control", "Experimental"))
-factor_gender <- factor(week2$gender, labels = c("Female", "Male", "Nonbinary"))
+rt_df <- read.csv(file ="data/week2.csv",header = TRUE)
+rt_df$condition <- factor(rt_df$condition, levels = c("A","B"), labels = c("Control", "Experimental"))
+rt_df$gender <- factor(rt_df$gender, levels = c("M","F","N"), labels = c("Male","Female", "Nonbinary"))
 
 # Analysis
 mean(rt_df$rt)
-rt_f_df <- subset(rt_df, gender == "F")
+rt_f_df <- rt_df[rt_df$gender == "Female",]
 hist(rt_f_df$rt)
 datasets <- list(rt_df, rt_f_df)
-datasets[[1]][,"rt"]
+datasets[[1]]$rt
 
